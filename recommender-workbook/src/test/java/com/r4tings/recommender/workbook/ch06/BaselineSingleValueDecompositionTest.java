64240da56e35c3ff472251fa98e2e435e57d49e0
Kim, Dong-Sup <31362557+r4tings@users.noreply.github.com>
2023-10-15 23:26:04 +0900

    Licensing updates

--- recommender-workbook/src/test/java/com/r4tings/recommender/workbook/ch06/BaselineSingleValueDecompositionTest.java
+++ recommender-workbook/src/test/java/com/r4tings/recommender/workbook/ch06/BaselineSingleValueDecompositionTest.java
@@ -1,5 +1,5 @@
 /*
- * The Apache License 2.0  Copyright (c) 2023 r4tings.com
+ * The Apache License 2.0 Copyright (c) 2023 r4tings.com and contributors
  * https://github.com/r4tings/r4tings-recommender/LICENSE.md
  */
 package com.r4tings.recommender.workbook.ch06;
